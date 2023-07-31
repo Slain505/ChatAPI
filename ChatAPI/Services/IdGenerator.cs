28054b7f38fe82310eac2c009678a16e871f54b0
Vladyslav Yefimenko <vlady@braingames.net>
2023-07-31 21:55:06 +0200

    Simple ID generator

--- ChatAPI/Services/IdGenerator.cs
+++ ChatAPI/Services/IdGenerator.cs
@@ -2,6 +2,17 @@ namespace ChatAPI.Services
 {
     public class IdGenerator
     {
+        private int lastUsedId;
         
+        public IdGenerator(int initialId = 0)
+        {
+            lastUsedId = initialId;
+        }
+        
+        public int GetNextId()
+        { 
+            lastUsedId++;
+            return lastUsedId;
+        }
     }
 }
